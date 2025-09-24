072a6b1bc8cb33202928bb4d7e6493105ba83345
Qnweii <QWONG011@e.ntu.edu.sg>
2025-09-24 14:57:19 +0800

    Added on: Lab2 submission for user description

--- Main Code/SkillsBridge SG/Frontend/src/Pages/Dashboard.jsx
+++ Main Code/SkillsBridge SG/Frontend/src/Pages/Dashboard.jsx
@@ -9,7 +9,6 @@ export default function Dashboard(){
         { id: 1, qualific:"Diploma in", courseName:"Accountancy", institution:"Ngee Ann Polytechnic", schoolCtg:"School of Business & Accountancy", courseDesr:"Aims to equip students with essential business knowledge and specialised training in accountancy and financial management to pursue a career in the Accountancy Sector.", courseType:"Part-Time"},
         { id: 2, qualific:"Degree in", courseName:"Sociology", institution:"National University of Singapore", schoolCtg:"Department of Sociology", courseDesr:"Aims to equip students with essential business knowledge and specialised training in arts and social politics.", courseType:"Full-Time"},
         { id: 3, qualific:"Degree in", courseName:"Sociology", institution:"National University of Singapore", schoolCtg:"Department of Sociology", courseDesr:"Aims to equip students with essential business knowledge and specialised training in arts and social politics.", courseType:"Full-Time"},
-        { id: 4, qualific:"Degree in", courseName:"Sociology", institution:"National University of Singapore", schoolCtg:"Department of Sociology", courseDesr:"Aims to equip students with essential business knowledge and specialised training in arts and social politics.", courseType:"Full-Time"},
     ]
 
     {/* Frontend */}
