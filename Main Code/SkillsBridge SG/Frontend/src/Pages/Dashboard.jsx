a25a523d0864336ad5bc3af8fd21570f59c15747
Loong <105075744+tebii@users.noreply.github.com>
2025-09-30 18:46:11 +0800

    Add backend access to dashboard

--- Main Code/SkillsBridge SG/Frontend/src/Pages/Dashboard.jsx
+++ Main Code/SkillsBridge SG/Frontend/src/Pages/Dashboard.jsx
@@ -3,17 +3,35 @@ import { Link } from "react-router-dom";
 import CourseCard from '../Components/CourseCard';
 import { useEffect, useState } from 'react';
 
+// Computed in frontend, probably should compute in backend instead?
+function getQualific(level) {
+  switch(level) {
+    case "poly": return "Diploma";
+    case "uni": return "Degree";
+    case "jc": return "A-Level";
+    default: return "";
+  }
+}
 export default function Dashboard(){  
     {/* Define functionality / datasets */}
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+      fetch("http://localhost:8000/api/courses/")
+      .then((res) => res.json())
+      .then((data) => setCourses(data))
+      .catch((err) => console.error("Error fetching courses:", err));
+  }, []);
+
+    
 
 
-
-    const populateCourseCards = [
+/*
+   const populateCourseCards = [
         { id: 1, qualific:"Diploma in", courseName:"Accountancy", institution:"Ngee Ann Polytechnic", schoolCtg:"School of Business & Accountancy", courseDesr:"Aims to equip students with essential business knowledge and specialised training in accountancy and financial management to pursue a career in the Accountancy Sector.", courseType:"Part-Time"},
         { id: 2, qualific:"Degree in", courseName:"Sociology", institution:"National University of Singapore", schoolCtg:"Department of Sociology", courseDesr:"Aims to equip students with essential business knowledge and specialised training in arts and social politics.", courseType:"Full-Time"},
         { id: 3, qualific:"Degree in", courseName:"Sociology", institution:"National University of Singapore", schoolCtg:"Department of Sociology", courseDesr:"Aims to equip students with essential business knowledge and specialised training in arts and social politics.", courseType:"Full-Time"},
     ]
-
+*/
     {/* Frontend */}
     
     return (
@@ -23,15 +41,15 @@ export default function Dashboard(){
             </div>
             
             <div id='cards'>
-                {populateCourseCards.map((course) => (
+                {courses.map((course) => (
                     <CourseCard 
                     key={course.id}
-                    qualific={course.qualific}
-                    courseName={course.courseName}
+                    qualific={getQualific(course.level)}
+                    courseName={course.course_name}
                     institution={course.institution}
-                    schoolCtg={course.schoolCtg}
-                    courseDesr={course.courseDesr}
-                    courseType={course.courseType}
+                    schoolCtg={course.school}
+                    courseDesr={course.course_description}
+                    courseType="Full-Time"
                 />
                 ))}
             </div>
